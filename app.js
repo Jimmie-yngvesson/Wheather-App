d5cf33c1afc88ab354ebb9014136d25a2dd15b63
Jimmie-yngvesson <125512498+Jimmie-yngvesson@users.noreply.github.com>
2023-03-02 23:02:51 +0100

    Uppdaterat funktioner

--- app.js
+++ app.js
@@ -1,41 +1,71 @@
 window.addEventListener("load", ()=>{
     let long;
     let lat;
-    let temperaturDescription = document.querySelector(".temperture-description")
-    let temperaturDegree = document.querySelector(".temperture-degree")
-    let locationTimeZone = document.querySelector(".location-timezone")
+    const temperaturDescription = document.querySelector(".temperture-description")
+    const temperaturDegree = document.querySelector(".temperture-degree")
+    const locationTimeZone = document.querySelector(".location-timezone")
+    const iconEl = document.getElementById("icon")
+    const inputEl = document.getElementById("locationInput")
+    let localTime = document.getElementById("currentTime")
 
+    
     if(navigator.geolocation)
     {
-        navigator.geolocation.getCurrentPosition(position =>{
+            navigator.geolocation.getCurrentPosition(position =>
+        {
             long = position.coords.longitude
             lat = position.coords.latitude
 
             const proxy = "https://cors-anywhere.herokuapp.com/"
-            const api = `${proxy}http://api.weatherapi.com/v1/current.json?key=1c90130abf8341a1b1d205819232802&q=${lat},${long}`
+            let api = `${proxy}http://api.weatherapi.com/v1/current.json?key=1c90130abf8341a1b1d205819232802&q=${lat},${long}`
+            getWheather(api)
             //console.log(api)
-            
-            
-            fetch(api)
-            .then(response => {
-              return response.json() 
+           
+            inputEl.addEventListener("click" ,function()
+            {
+                inputEl.value =""
             })
-            .then(data => {
-                console.log(data)
-                const temp = data.current.temp_c;
-                const summary = data.current.condition.text;
-                //const location = data.current.temp_c
-                // Set DOM element frpm API
-                temperaturDegree.textContent = temp + " C"
-                temperaturDescription.textContent = summary
-                locationTimeZone.textContent = data.location.name
+            //letar upp inputfältet och lyssnar om trycker Enter. Värdet fråmn inputfältest ska sedan sparas
+            inputEl.addEventListener('change', (e) => 
+            {
+                let city = e.currentTarget.value
+                api = `${proxy}http://api.weatherapi.com/v1/current.json?key=1c90130abf8341a1b1d205819232802&q=${city}`
+                getWheather(api)
+               
                 
-            })
+             });
+                function getWheather(api)  
+                {  
+                fetch(api)
+                .then(response => {
+                return response.json() 
+                })
+                .then(data => {
+                    console.log(data)
+                    const temp = data.current.temp_c;
+                    if(temp < 0)
+                    {
+                        temperaturDegree.style.color = "blue"
+                    }
+                    else{
+                        temperaturDegree.style.color = "red"
+                    }
+                    console.log(temp)
+                    const summary = data.current.condition.text;
+                   
+                    // Set DOM element from API
+                    temperaturDegree.textContent = temp + " C"
+                    temperaturDescription.textContent = summary
+                    locationTimeZone.textContent = data.location.name
+                    iconEl.innerHTML = `<img src="${data.current.condition.icon}" style="height:100px"/>`
+                    localTime.textContent = "Lokal tid: " + data.location.localtime.slice(11)
+                   
+                    
+                    
+                })
+            }
+            
         })
        
     }
-        function setIcons(icon, iconId)
-        {
-            const skycons = new Skycons({color:"white"})
-        }
 })
